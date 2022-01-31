aa11758796701438d98eca77d1534f0aaabe4325
DharmaCaesar <aquadharmasidha@gmail.com>
2022-01-31 15:42:16 +0700

    Config edit form outlet

--- resources/views/preload/default.blade.php
+++ resources/views/preload/default.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Document</title>
 </head>
@@ -13,6 +14,7 @@
     </div>
 
     <script src="{{ asset("js/app.js") }}"></script>
+    <script src="{{ asset("js/grubbywash.js") }}"></script>
 
     <script src="{{ asset("js/outlet.js") }}"></script>
 
