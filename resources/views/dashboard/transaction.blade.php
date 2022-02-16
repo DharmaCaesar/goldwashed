b6d4533920bca1e3f59c8108dc6fb70845990ccc
DharmaCaesar <aquadharmasidha@gmail.com>
2022-02-16 09:47:20 +0700

    Adding features Transaction

--- resources/views/dashboard/transaction.blade.php
+++ resources/views/dashboard/transaction.blade.php
@@ -78,17 +78,6 @@
                 <td></td>
             </tr>
         </tbody>
-        <tfoot>
-            <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Job</th>
-                <th>company</th>
-                <th>location</th>
-                <th>Last Login</th>
-                <th>Favorite Color</th>
-            </tr>
-        </tfoot>
     </table>
 </div>
 {{-- BAGIAN AKHIR TABLE --}}
@@ -96,7 +85,7 @@
 {{-- BAGIAN AWAL CUSTOMER & SELLER INFO --}}
 <div class="flex flex-row my-4">
     <div class="flex-1 mx-4 w-full">
-        <div class="collapse w-96 border rounded-box border-base-300 collapse-arrow">
+        <div class="collapse border rounded-box border-base-300 collapse-arrow">
             <input type="checkbox">
             <div class="collapse-title text-xl font-medium text-center">
                 Customer Information
@@ -144,7 +133,7 @@
     </div>
 
     <div class="flex-1 mx-4 w-full">
-        <div class="collapse w-96 border rounded-box border-base-300 collapse-arrow">
+        <div class="collapse border rounded-box border-base-300 collapse-arrow">
             <input type="checkbox">
             <div class="collapse-title text-xl font-medium text-center">
                 Seller Information
@@ -153,37 +142,36 @@
                 <div>
                     <div class="form-control">
                         <label class="label">
-                            <span class="label-text">Customer Name</span>
-                        </label>
-                        <div class="input-group">
-                            <input type="text" name="name" placeholder="Search…" class="input input-bordered w-full" id="nameInput" readonly>
-                            <button class="btn btn-square">Find</button>
-                        </div>
-                    </div>
-                    <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Address</span>
-                        </label>
-                        <input type="text" name="address" placeholder="Location name"
-                            class="input input-secondary input-bordered w-full" id="addressInput" readonly>
-                    </div>
-                    <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Contact number</span>
-                        </label>
-                        <div class="input-group">
-                            <span>+62</span>
-                            <input type="text" name="number" placeholder="Phone Number"
-                                class="input input-accent input-bordered w-full" id="numberInput" readonly>
-                        </div>
-                    </div>
-                    <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Gender</span>
+                            <span class="label-text">Transaction Type</span>
                         </label>
                         <div class="flex-row">
-                            <input type="text" name="gender" placeholder="Gender"
-                                class="input input-accent input-bordered w-full" id="genderInput" readonly>
+                            <select name="type" class="select select-bordered w-full" id="typeInput">
+                                <option disabled="disabled" selected="selected">Choose Transaction Type will Used</option>
+                                <option value="paynow">Pay Now</option>
+                                <option value="paylater">Pay Later</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text">Deadline Date</span>
+                        </label>
+                        <input type="date" name="date" placeholder=""
+                            class="input input-secondary input-bordered w-full bg-neutral-content text-black" id="dateInput">
+                    </div>
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text">Discount</span>
+                        </label>
+                        <div class="flex-row">
+                            <select name="disc" class="select select-bordered w-full" id="discInput">
+                                <option disabled="disabled" selected="selected">Select Discount</option>
+                                <option value="10">10% Discount</option>
+                                <option value="20">20% Discount</option>
+                                <option value="30">30% Discount</option>
+                                <option value="40">40% Discount</option>
+                                <option value="50">50% Discount</option>
+                            </select>
                         </div>
                     </div>
                 </div>
